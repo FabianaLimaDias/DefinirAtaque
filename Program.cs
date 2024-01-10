663d0789038b033768d71597d6dadab6816053fd
FabianaDias <129968421+FabianaLimaDias@users.noreply.github.com>
2024-01-10 17:14:54 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+
+namespace EstudosDIO { 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            JogoDIO jogador = new JogoDIO();
+            jogador.Atacar ();
+
+            Console.ReadKey();
+        }
+
+
+
+            }
+
+}
